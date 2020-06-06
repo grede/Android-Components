6c0318bd0b821f207d3fe4817c744a79cc7913e5
Kirill Rozhenkov <gredex0r@gmail.com>
2020-06-06 17:07:59 +0200

    Add ViewGroup inflate extension function

--- ViewExtensions/src/main/kotlin/app/xorium/androidcomponents/viewextensions/View.kt
+++ ViewExtensions/src/main/kotlin/app/xorium/androidcomponents/viewextensions/View.kt
@@ -1,7 +1,10 @@
 package app.xorium.androidcomponents.viewextensions
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
 
 fun <T : View> View.lazyViewById(@IdRes id: Int): Lazy<T> {
     return lazy(LazyThreadSafetyMode.NONE) { findViewById<T>(id) }
@@ -22,3 +25,5 @@ fun View.gone() {
 fun View.visible() {
     visibility = View.VISIBLE
 }
+
+fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean = false): View = LayoutInflater.from(context).inflate(layout, this, attachToRoot)
