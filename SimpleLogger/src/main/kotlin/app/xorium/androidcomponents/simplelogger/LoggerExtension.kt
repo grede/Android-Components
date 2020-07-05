825cf9c02acfaec9f5ba9871eb0b4bd2749eb047
Kirill Rozhenkov <gredex0r@gmail.com>
2020-07-05 16:32:17 +0200

    Add more flexible extensions for logger functions

--- SimpleLogger/src/main/kotlin/app/xorium/androidcomponents/simplelogger/LoggerExtension.kt
+++ SimpleLogger/src/main/kotlin/app/xorium/androidcomponents/simplelogger/LoggerExtension.kt
@@ -4,15 +4,27 @@ package app.xorium.androidcomponents.simplelogger
 val Any.tag: String
     get() = this.javaClass.simpleName
 
-fun Any.logV(tag: String = this.tag, msg: String) {
+fun Any.logV(msg: String) {
     SimpleLogger.logVerbose(tag, msg)
 }
 
-fun Any.logD(tag: String = this.tag, msg: String) {
+fun Any.logV(tag: String, msg: String) {
+    SimpleLogger.logVerbose(tag, msg)
+}
+
+fun Any.logD(msg: String) {
     SimpleLogger.logDebug(tag, msg)
 }
 
-fun Any.logW(tag: String = this.tag, msg: String) {
+fun Any.logD(tag: String, msg: String) {
+    SimpleLogger.logDebug(tag, msg)
+}
+
+fun Any.logW(msg: String) {
+    SimpleLogger.logWarn(tag, msg)
+}
+
+fun Any.logW(tag: String, msg: String) {
     SimpleLogger.logWarn(tag, msg)
 }
 
@@ -20,6 +32,10 @@ fun Any.logE(tag: String = this.tag, msg: String, error: Throwable? = null) {
     SimpleLogger.logError(tag, msg, error)
 }
 
-fun Any.logE(tag: String = this.tag, error: Throwable? = null) {
+fun Any.logE(tag: String = this.tag, error: Throwable) {
+    SimpleLogger.logError(tag, null, error)
+}
+
+fun Any.logE(error: Throwable) {
     SimpleLogger.logError(tag, null, error)
 }
